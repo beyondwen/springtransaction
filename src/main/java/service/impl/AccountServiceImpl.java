3e30cf90716e4ad8587ca32fb7675d15cb98d79c
lenovo <921790476@qq.com>
2016-10-06 16:56:16 +0800

    声明式事务管理-修改代码

--- src/main/java/service/impl/AccountServiceImpl.java
+++ src/main/java/service/impl/AccountServiceImpl.java
@@ -25,7 +25,7 @@ public class AccountServiceImpl implements AccountService {
 
     public void transfer(String from, String to, Double money) {
         accountDao.transferOut(from, money);
-        int i = 1 / 0;
+        //int i = 1 / 0;
         accountDao.transferIn(to, money);
     }
 }
