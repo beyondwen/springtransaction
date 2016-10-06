7eeaab8b74d1bf256fee2429671184ad93750d92
lenovo <921790476@qq.com>
2016-10-06 16:37:03 +0800

    编码式事务

--- src/main/java/service/impl/AccountServiceImpl.java
+++ src/main/java/service/impl/AccountServiceImpl.java
@@ -1,6 +1,9 @@
 package service.impl;
 
 import dao.AccountDao;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 import service.AccountService;
 
 /**
@@ -9,13 +12,26 @@ import service.AccountService;
 public class AccountServiceImpl implements AccountService {
 
     private AccountDao accountDao;
+    private TransactionTemplate transactionTemplate;
+
 
     public void setAccountDao(AccountDao accountDao) {
         this.accountDao = accountDao;
     }
 
-    public void transfer(String from, String to, Double money) {
-        accountDao.transferOut(from, money);
-        accountDao.transferIn(to, money);
+    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+        this.transactionTemplate = transactionTemplate;
+    }
+
+    public void transfer(final String from, final String to, final Double money) {
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                accountDao.transferOut(from, money);
+                //int i = 1 / 0;
+                accountDao.transferIn(to, money);
+            }
+        });
     }
 }
+
