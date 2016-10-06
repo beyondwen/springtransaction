cc9dcdf9b5ccd4d6afb1db24b5014d4d55405f26
lenovo <921790476@qq.com>
2016-10-06 21:20:52 +0800

    声明式事务管理--注解方式

--- src/main/java/service/impl/AccountServiceImpl.java
+++ src/main/java/service/impl/AccountServiceImpl.java
@@ -2,6 +2,7 @@ package service.impl;
 
 import dao.AccountDao;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 import service.AccountService;
@@ -9,6 +10,7 @@ import service.AccountService;
 /**
  * Created by lenovo on 2016/10/06.
  */
+@Transactional
 public class AccountServiceImpl implements AccountService {
 
     private AccountDao accountDao;
@@ -24,14 +26,9 @@ public class AccountServiceImpl implements AccountService {
     }
 
     public void transfer(final String from, final String to, final Double money) {
-        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-            @Override
-            protected void doInTransactionWithoutResult(TransactionStatus status) {
-                accountDao.transferOut(from, money);
-                //int i = 1 / 0;
-                accountDao.transferIn(to, money);
-            }
-        });
+        accountDao.transferOut(from, money);
+        //int i = 1 / 0;
+        accountDao.transferIn(to, money);
     }
 }
 
